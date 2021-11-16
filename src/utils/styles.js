cadb146eaa2d4c379328729b7c07b07af433817a
wazimuhb <hesbonosoro1@gmail.com>
2021-11-17 02:16:20 +0300

    global styles

---
+++ src/utils/styles.js
@@ -0,0 +1,16 @@
+export const colors = {
+	mainWhite: `#fff`,
+	mainBlack: `#262626`,
+	mainYellow: `#d2aa5c`,
+	mainYellow2: `#F2AF29`,
+	mainGrey: `#474747`,
+};
+export const transDefault = 'transition:all 0.5s ease-in-out';
+
+export const transObject = ({
+	property = 'all',
+	time = '0.5s',
+	type = 'ease-in-out',
+}) => {
+	return `transition:${property} ${time} ${type}`;
+};
