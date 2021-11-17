17d37cbcaeb49b863df73578e3ec8bb299614dd0
wazimuhb <hesbonosoro1@gmail.com>
2021-11-18 01:35:53 +0300

    updated NavbarLinks component

--- src/components/globals/NavbarLinks.js
+++ src/components/globals/NavbarLinks.js
@@ -27,35 +27,35 @@ class NavbarLinks extends Component {
 }
 
 const LinkWrapper = styled.ul`
-	li{
+	li {
 		list-style-type: none;
 	}
-	.nav-link{
-		display:block;
-		text-decoration:none;
-		padding:0.5rem 1rem 0.5rem 1rem;
+	.nav-link {
+		display: block;
+		text-decoration: none;
+		padding: 0.5rem 1rem 0.5rem 1rem;
 		color: ${styles.colors.mainGrey};
-		font-weight:700;
-		text-transform:capitalize;
-		cursor:pointer;
+		font-weight: 700;
+		text-transform: capitalize;
+		cursor: pointer;
 		${styles.transDefault};
 
-		&:hover{
+		&:hover {
 			background: ${styles.colors.mainGrey};
-			color:${styles.colors.mainYellow};
-			padding:0.5rem 1rem 0.5rem 1.3rem;
+			color: ${styles.colors.mainYellow};
+			padding: 0.5rem 1rem 0.5rem 1.3rem;
 		}
 	}
 	height: ${props => (props.open ? '152px' : '0px')};
 	overflow: hidden;
 	${styles.transObject({ time: '1s' })};
 
-	@media (min-width: 768px){
+	@media (min-width: 768px) {
 		height: auto;
 		display: flex;
 		margin: 0 auto;
 
-		.nav-link:hover{
+		.nav-link:hover {
 			background: ${styles.colors.mainWhite};
 			padding: 0.5rem 1rem 0.5rem 1rem;
 		}
