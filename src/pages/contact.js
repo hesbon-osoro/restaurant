db6484a39025e7018e012d8c20911dda8fad7728
wazimuhb <hesbonosoro1@gmail.com>
2021-11-18 01:29:32 +0300

    updated contact page with necessary components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
+import { Banner, PageHeader } from '../utils';
+import contactImg from '../images/bcg/contactBcg.jpg';
 
 const Contact = () => {
-    return (
-        <Layout>
-            <Seo title="Contact" />
-            <h3>Contact page</h3>
-        </Layout>
-    );
-}
+	return (
+		<Layout>
+			<Seo title="Contact" />
+			<PageHeader img={contactImg}>
+				<Banner title='contact us' subtitle="Let's get in touch"/>
+			</PageHeader>
+		</Layout>
+	);
+};
 
 export default Contact;
