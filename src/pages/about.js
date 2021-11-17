8ad14bbc60cf4fcaa20a6c66fb71df2d943969c7
wazimuhb <hesbonosoro1@gmail.com>
2021-11-18 01:29:59 +0300

    updated about page with necessary components

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
+import { Banner, PageHeader } from '../utils';
+import aboutImg from '../images/bcg/aboutBcg.jpg';
 
 const About = () => {
-    return (
-        <Layout>
-            <Seo title="About" />
-            <h3>About page</h3>
-        </Layout>
-    );
-}
+	return (
+		<Layout>
+			<Seo title="About" />
+			<PageHeader img={aboutImg}>
+				<Banner title='about us' subtitle='a little about us'/>
+			</PageHeader>
+		</Layout>
+	);
+};
 
 export default About;
