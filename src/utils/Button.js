6ccfa35de70155d201791f330c998d4c075d48e4
wazimuhb <hesbonosoro1@gmail.com>
2021-11-18 01:54:46 +0300

    created a Banner Button

---
+++ src/utils/Button.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+import {styles } from '../utils'
+
+const BannerButton = styled.button`
+    display:block;
+    color: ${styles.colors.mainWhite};
+    background: transparent;
+    padding: 0.5rem 1rem;
+    text-transform: uppercase;
+    font-size: 1.5rem;
+    letter-spacing: 0.5rem;
+    font-weight: 700;
+    ${styles.border({color: `{styles.colors.mainWhite}`})};
+    margin-top: 1rem;
+    ${styles.transObject({})};
+    &:hover{
+        background: ${styles.colors.mainWhite};
+        color: ${styles.colors.mainBlack};
+        cursor: pointer;
+    }
+`
+
+export {BannerButton}
