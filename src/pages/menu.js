3f103825df67ef1bbf122190401ee3f3d314368f
wazimuhb <hesbonosoro1@gmail.com>
2021-11-18 01:29:44 +0300

    updated menu page with necessary components

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
+import { Banner, PageHeader } from '../utils';
+import menuImg from '../images/bcg/menuBcg.jpg';
 
 const Menu = () => {
-    return (
-        <Layout>
-            <Seo title="Menu" />
-            <h3>Menu page</h3>
-        </Layout>
-    );
-}
+	return (
+		<Layout>
+			<Seo title="Menu" />
+			<PageHeader img={menuImg}>
+				<Banner title="our menu" subtitle="the best in town" />
+			</PageHeader>
+		</Layout>
+	);
+};
 
 export default Menu;
