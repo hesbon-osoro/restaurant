0ae05cf47c4f28c866ff28df24ff497acb1b1eca
wazimuhb <hesbonosoro1@gmail.com>
2021-11-22 01:16:00 +0300

    SectionButton styled component

--- src/utils/Button.js
+++ src/utils/Button.js
@@ -19,5 +19,10 @@ const BannerButton = styled.button`
 		cursor: pointer;
 	}
 `;
-
-export { BannerButton };
+const SectionButton = styled(BannerButton)`
+color:${styles.colors.mainBlack};
+${styles.border({color: `${styles.colors.mainBlack}`})};
+&:hover{background: ${styles.colors.mainBlack}};
+color: ${styles.colors.mainYellow};
+cursor:pointer;`
+export { BannerButton, SectionButton };
