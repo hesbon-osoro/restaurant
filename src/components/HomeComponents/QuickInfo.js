9a650bfd823cba4ded69053bf22533a2f7b1ac7e
wazimuhb <hesbonosoro1@gmail.com>
2021-11-22 00:04:31 +0300

    QuickInfo home component intialization

---
+++ src/components/HomeComponents/QuickInfo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Section, Title } from '../../utils';
+
+export default class QuickInfo extends Component {
+	render() {
+		return (
+			<section>
+				<Title message="let us tell you" title="our mission" />
+			</section>
+		);
+	}
+}
