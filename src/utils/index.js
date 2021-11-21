22dcbe6ac73bb88d0eb2515c7c2a7d88e8d4adb6
wazimuhb <hesbonosoro1@gmail.com>
2021-11-22 01:19:30 +0300

    exporting SectionButton styled-component

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,17 @@
 import * as styles from './styles';
 import { HomeHeader, PageHeader } from './Header';
 import { Banner } from './Banner';
-import { BannerButton } from './Button';
+import { BannerButton, SectionButton } from './Button';
 import { Title } from './Title';
 import { Section } from './Section';
 
-export { Title, Section, BannerButton, Banner, HomeHeader, PageHeader, styles };
+export {
+	SectionButton,
+	Title,
+	Section,
+	BannerButton,
+	Banner,
+	HomeHeader,
+	PageHeader,
+	styles,
+};
