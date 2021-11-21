936b2cf208adb06f9c17aed27db283c027f7af2d
wazimuhb <hesbonosoro1@gmail.com>
2021-11-22 01:07:20 +0300

    imported BannerButton and QuickInfo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Layout from '../components/layout';
-import { HomeHeader, Banner } from '../utils';
+import { HomeHeader, Banner, BannerButton } from '../utils';
 import img from '../images/bcg/homeBcg.jpg';
 import Seo from '../components/seo';
+import QuickInfo from '../components/HomeComponents/QuickInfo';
 
 const IndexPage = () => (
 	<Layout>
 		<Seo title="Home" />
 		<HomeHeader img={img}>
-			<Banner title="Fine Dining" subtitle="65, MG Road-Bangalore, KA"></Banner>
+			<Banner title="Fine Dining" subtitle="65, MG Road-Bangalore, KA">
+				<BannerButton style={{ margin: '2rem auto' }}>menu</BannerButton>
+			</Banner>
 		</HomeHeader>
+		<QuickInfo />
 	</Layout>
 );
 
