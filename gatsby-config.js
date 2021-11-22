9238ac2cf2dd0a5e24563c3fb6624ec7532728b2
wazimuhb <hesbonosoro1@gmail.com>
2021-11-22 23:19:23 +0300

    minor changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,4 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 module.exports = {
 	siteMetadata: {
 		title: `Restaurant Site`,
@@ -8,6 +9,22 @@ module.exports = {
 		twitterUsername: `@wazimuhb`,
 	},
 	plugins: [
+		{
+			resolve: 'gatsby-plugin-web-font-loader',
+			options: {
+				typekit: {
+					id: process.env.TYPEKIT_ID,
+				},
+			},
+		},
+		{
+			resolve: `gatsby-source-contentful`,
+			options: {
+				spaceId: process.env.SPACE_ID,
+				// Learn about environment variables: https://gatsby.dev/env-vars
+				accessToken: process.env.ACCESS_TOKEN,
+			},
+		},
 		`gatsby-plugin-react-helmet`,
 		`gatsby-plugin-image`,
 		{
