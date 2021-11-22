c02f5c17105c351cf710feb2d98ce34b6a5d2766
wazimuhb <hesbonosoro1@gmail.com>
2021-11-23 01:45:16 +0300

    required text update

--- src/components/HomeComponents/QuickInfo.js
+++ src/components/HomeComponents/QuickInfo.js
@@ -10,10 +10,9 @@ export default class QuickInfo extends Component {
 				<Title message="let us tell you" title="our mission" />
 				<QuickInfoWrapper>
 					<p className="text">
-						Our mission is to serve you authetic dishes from north karnataka.
-						these dishes are created by our top chefs, from the region. Some of
-						our awesome dishes are - Menther kadabu, Pundi Soppu, Jolad Rotti,
-						Rogi Ambali, Nargis Man Dak ki, Mudde, Girmit and many more.
+						Our mission is to provide happiness and joy through food to every
+						customer who chooses to dine at Restaurant, while not exceeding,
+						making the size of a pin hole on their pockets.
 					</p>
 				</QuickInfoWrapper>
 				<Link to="/about/" style={{ textDecoration: 'none' }}>
