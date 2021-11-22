50451c264541d14269e835736a62c6243ba54436
wazimuhb <hesbonosoro1@gmail.com>
2021-11-23 01:46:58 +0300

    Contact component into use

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,16 +3,18 @@ import Layout from '../components/layout';
 import Seo from '../components/seo';
 import { Banner, PageHeader } from '../utils';
 import contactImg from '../images/bcg/contactBcg.jpg';
+import Contact from '../components/Contact/Contact';
 
-const Contact = () => {
+const ContactPage = () => {
 	return (
 		<Layout>
 			<Seo title="Contact" />
 			<PageHeader img={contactImg}>
 				<Banner title="contact us" subtitle="Let's get in touch" />
 			</PageHeader>
+			<Contact />
 		</Layout>
 	);
 };
 
-export default Contact;
+export default ContactPage;
