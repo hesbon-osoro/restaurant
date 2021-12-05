c9b6aa94e0aa6e9519f95b352896b7c14e547b33
wazimuhb <hesbonosoro1@gmail.com>
2021-12-05 16:41:32 +0300

    menu item added

--- src/components/globals/navbar/NavbarLinks.js
+++ src/components/globals/navbar/NavbarLinks.js
@@ -9,7 +9,8 @@ class NavbarLinks extends Component {
 		links: [
 			{ id: 0, path: '/', name: 'home' },
 			{ id: 1, path: '/about/', name: 'about' },
-			{ id: 2, path: '/contact/', name: 'contact' },
+			{ id: 2, path: '/menu/', name: 'menu' },
+			{ id: 3, path: '/contact/', name: 'contact' },
 		],
 	};
 	render() {
