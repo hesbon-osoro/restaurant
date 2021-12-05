ec5bb4cb791ffce5ac526c4b42ea9c327e05020a
wazimuhb <hesbonosoro1@gmail.com>
2021-12-05 16:39:56 +0300

    fixing the UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,20 +4,23 @@ import { HomeHeader, Banner, BannerButton } from '../utils';
 import img from '../images/bcg/homeBcg.jpg';
 import Seo from '../components/seo';
 import QuickInfo from '../components/HomeComponents/QuickInfo';
-import Gallery from '../components/HomeComponents/Gallery';
-import Menu from '../components/HomeComponents/Menu';
+// import Gallery from '../components/HomeComponents/Gallery';
+// import Menu from '../components/HomeComponents/Menu';
+import { Link } from 'gatsby';
 
 const IndexPage = () => (
 	<Layout>
 		<Seo title="Home" />
 		<HomeHeader img={img}>
 			<Banner title="Fine Dining" subtitle="65, MG Road-Bangalore, KA">
-				<BannerButton style={{ margin: '2rem auto' }}>menu</BannerButton>
+				<Link to="/menu/" style={{ textDecoration: 'none' }}>
+					<BannerButton style={{ margin: '2rem auto' }}>menu</BannerButton>
+				</Link>
 			</Banner>
 		</HomeHeader>
 		<QuickInfo />
-		<Gallery />
-		<Menu />
+		{/* <Gallery /> */}
+		{/* <Menu /> */}
 	</Layout>
 );
 
